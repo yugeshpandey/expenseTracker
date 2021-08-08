0ea150e86ea983a0ca9d2227b85daf4629869d1f
Yugesh Pandey <27809103+yugeshpandey@users.noreply.github.com>
2021-08-08 09:05:06 -0400

    Added API controller

--- src/main/java/com/yugeshpandey/expense/model/User.java
+++ src/main/java/com/yugeshpandey/expense/model/User.java
@@ -1,12 +1,9 @@
 package com.yugeshpandey.expense.model;
 
-import java.util.Set;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -30,9 +27,28 @@ public class User {
 	private String name;
 	@Getter @Setter
 	private String email;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-	@OneToMany
-	private Set<Category> categories;
+	/*
+	 * @OneToMany private Set<Category> categories;
+	 */
 	
 
 }
